8b2bc12f0f6a0a6991b096948537765f9a127f56
Ahmad Ragab <ahmad.ragab@ironnetcybersecurity.com>
2022-12-02 15:49:16 -0500

    feat: allow custom headers in reports

--- test/context.js
+++ test/context.js
@@ -10,6 +10,8 @@ test('context', async assert => {
   process.env['INPUT_SHOW-DIFF'] = 'false'
   process.env['INPUT_SHOW-PLAN'] = 'true'
   process.env['INPUT_GITHUB-TOKEN'] = 'abc'
+  process.env['INPUT_REMOVE-STALE-REPORTS'] = 'true'
+  process.env['INPUT_CUSTOM-HEADER'] = 'abc'
 
   process.env['INPUT_TERRAFORM-TEXT'] = join(root, 'fixtures/terraform.txt')
   process.env['INPUT_TERRAFORM-JSON'] = join(root, 'fixtures/terraform.json')
@@ -24,4 +26,6 @@ test('context', async assert => {
   assert.equal(data.token, 'abc')
   assert.match(data.textPath, 'fixtures/terraform.txt')
   assert.match(data.jsonPath, 'fixtures/terraform.json')
+  assert.match(data.removeStaleReports, 'true')
+  assert.match(data.customHeader, 'abc')
 })
--- test/report.js
+++ test/report.js
@@ -8,18 +8,8 @@ const root = fileURLToPath(new URL('./', import.meta.url))
 
 test('context', async assert => {
   // set up
-  process.env['INPUT_SHOW-DIFF'] = 'true'
-  process.env['INPUT_SHOW-PLAN'] = 'true'
-  process.env['INPUT_GITHUB-TOKEN'] = 'abc'
-
-  process.env['INPUT_TERRAFORM-TEXT'] = join(root, 'fixtures/terraform.txt')
-  process.env['INPUT_TERRAFORM-JSON'] = join(root, 'fixtures/terraform.json')
-
-  process.env.GITHUB_EVENT_PATH = join(root, 'fixtures/opened.payload.json')
-
   process.env.GITHUB_REPOSITORY = 'foo/bar'
   process.env.GITHUB_SHA = 'sha'
-  process.env.GITHUB_REF = 'ref'
   process.env.GITHUB_RUN_ID = '1000'
 
   const fixture = await readFile(join(root, 'fixtures/comment.md'))
@@ -33,7 +23,8 @@ test('context', async assert => {
     diff: {
       patches: ['foo'],
       summary: { create: 0, update: 1, delete: 0 }
-    }
+    },
+    customHeader: ':robot: *Terraform Report* - This is a custom header'
   }
 
   report(data)
